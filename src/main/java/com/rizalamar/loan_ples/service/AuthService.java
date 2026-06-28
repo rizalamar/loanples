0ec36c13d32161d5b3a084ad203235c744004ba2
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-06-28 20:00:47 +0700

    feat: implement login regiter logic update: user repository by adding duplicate email validation

---
+++ src/main/java/com/rizalamar/loan_ples/service/AuthService.java
@@ -0,0 +1,77 @@
+package com.rizalamar.loan_ples.service;
+
+import com.rizalamar.loan_ples.domain.Role;
+import com.rizalamar.loan_ples.domain.User;
+import com.rizalamar.loan_ples.dto.auth.AuthResponse;
+import com.rizalamar.loan_ples.dto.auth.LoginRequest;
+import com.rizalamar.loan_ples.dto.auth.RegisterRequest;
+import com.rizalamar.loan_ples.repository.UserRepository;
+import com.rizalamar.loan_ples.security.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse register(RegisterRequest request) {
+        if(userRepository.existsByEmail(request.email())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email already exists");
+        }
+
+        User user = User.builder()
+                .name(request.name())
+                .email(request.email())
+                .password(passwordEncoder.encode(request.password()))
+                .role(Role.BORROWER)
+                .build();
+
+        userRepository.save(user);
+
+        String token = jwtUtil.generateToken(user.getEmail());
+
+        return AuthResponse.builder()
+                .token(token)
+                .userId(user.getId())
+                .email(user.getEmail())
+                .role(user.getRole().name())
+                .build();
+    }
+
+    public AuthResponse login(LoginRequest request){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.email(),
+                        request.password()
+                )
+        );
+
+        User user = userRepository.findByEmail(request.email())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+
+        Map<String, Object> extraClaims = new HashMap<>();
+        extraClaims.put("name", user.getName());
+        extraClaims.put("role", user.getRole().name());
+
+        String token = jwtUtil.generateToken(extraClaims, user.getEmail());
+
+        return AuthResponse.builder()
+                .token(token)
+                .userId(user.getId())
+                .email(user.getEmail())
+                .role(user.getRole().name())
+                .build();
+    }
+}
