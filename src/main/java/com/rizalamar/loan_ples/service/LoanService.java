2c0b98c0d0cf893b92fd7fc53a446884767dc61b
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-06-30 17:05:48 +0700

    refactor: add method fund loan and generate date schedules

--- src/main/java/com/rizalamar/loan_ples/service/LoanService.java
+++ src/main/java/com/rizalamar/loan_ples/service/LoanService.java
@@ -1,25 +1,33 @@
 package com.rizalamar.loan_ples.service;
 
-import com.rizalamar.loan_ples.domain.Loan;
-import com.rizalamar.loan_ples.domain.LoanStatus;
-import com.rizalamar.loan_ples.domain.User;
+import com.rizalamar.loan_ples.domain.*;
 import com.rizalamar.loan_ples.dto.loan.LoanRequest;
 import com.rizalamar.loan_ples.dto.loan.LoanResponse;
 import com.rizalamar.loan_ples.mapper.LoanMapper;
 import com.rizalamar.loan_ples.repository.LoanRepository;
+import com.rizalamar.loan_ples.repository.LoanScheduleRepository;
+import com.rizalamar.loan_ples.repository.WalletRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class LoanService {
 
     private final LoanRepository loanRepository;
+    private final WalletRepository walletRepository;
+    private final LoanScheduleRepository loanScheduleRepository;
     private final ValidationService validationService;
     private final LoanMapper loanMapper;
 
@@ -46,5 +54,63 @@ public class LoanService {
                 .stream()
                 .map(loanMapper::toResponse)
                 .toList();
+    }
+
+    @Transactional
+    public void fundLoan(User lender, UUID loanId){
+        // 1. Ambil data Loan dengan LOCK (supaya tidak diserobot)
+        Loan loan = loanRepository.findWithLockById(loanId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Loan not found"));
+
+        // 2. Validasi status
+        if(loan.getStatus() != LoanStatus.PENDING){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Loan is not pending");
+        }
+
+        // 3. Ambil wallet & cek saldo
+        Wallet lenderWallet = walletRepository.findByUser(lender)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Wallet not found"));
+
+        if(lenderWallet.getBalance().compareTo(loan.getPrincipalAmount()) < 0){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Insufficient balance");
+        }
+
+        // 4. Potong saldo
+        lenderWallet.setBalance(lender.getWallet().getBalance().subtract(loan.getPrincipalAmount()));
+        walletRepository.save(lenderWallet);
+
+        // 5. Update status loan
+        loan.setStatus(LoanStatus.DISBURSED);
+        loan.setLender(lender);
+        loanRepository.save(loan);
+
+        generateSchedules(loan);
+    }
+
+    private void generateSchedules(Loan loan){
+        BigDecimal principalAmount = loan.getPrincipalAmount();
+        BigDecimal interestRate = loan.getInterestRate();
+        Integer tenorMonths = loan.getTenorMonths();
+
+        BigDecimal monthlyPrincipal = principalAmount.divide(BigDecimal.valueOf(tenorMonths), 2, RoundingMode.HALF_UP);
+        BigDecimal monthlyInterest = principalAmount.multiply(interestRate);
+        BigDecimal totalMonthlyPayment = monthlyPrincipal.add(monthlyInterest);
+
+        LocalDate baseDate = LocalDate.now().plusMonths(1).withDayOfMonth(5);
+
+        for(int i = 1; i <= tenorMonths; i++){
+            LocalDate dueDate = baseDate.plusMonths(i - 1);
+            LocalDateTime dueDateTime = dueDate.atTime(23, 59, 59);
+
+            LoanSchedule schedule = LoanSchedule.builder()
+                    .loan(loan)
+                    .installmentNumber(i)
+                    .amountToPay(totalMonthlyPayment)
+                    .dueDate(dueDateTime)
+                    .isPaid(false)
+                    .build();
+
+            loanScheduleRepository.save(schedule);
+        }
     }
 }
