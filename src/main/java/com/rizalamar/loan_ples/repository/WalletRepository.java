16ab8d0414de756700f19d71725320bcfb2373e6
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-06-30 17:04:51 +0700

    refactor: add query method find by user

--- src/main/java/com/rizalamar/loan_ples/repository/WalletRepository.java
+++ src/main/java/com/rizalamar/loan_ples/repository/WalletRepository.java
@@ -1,5 +1,6 @@
 package com.rizalamar.loan_ples.repository;
 
+import com.rizalamar.loan_ples.domain.User;
 import com.rizalamar.loan_ples.domain.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,6 @@ import java.util.UUID;
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, UUID> {
     Wallet findByUserEmail(String email);
+
+    Optional<Wallet> findByUser(User user);
 }
