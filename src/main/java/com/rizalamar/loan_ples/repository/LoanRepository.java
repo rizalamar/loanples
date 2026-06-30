f23aec210967a7b8cb26c6d624b6829a36521c98
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-06-30 13:29:23 +0700

    update: loan repository by add query method exsits by user and status

--- src/main/java/com/rizalamar/loan_ples/repository/LoanRepository.java
+++ src/main/java/com/rizalamar/loan_ples/repository/LoanRepository.java
@@ -1,11 +1,15 @@
 package com.rizalamar.loan_ples.repository;
 
 import com.rizalamar.loan_ples.domain.Loan;
+import com.rizalamar.loan_ples.domain.LoanStatus;
+import com.rizalamar.loan_ples.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface LoanRepository extends JpaRepository<Loan, UUID> {
+    boolean existsByBorrowerAndStatus(User borrower, LoanStatus status);
 }
