a79240b63002ca5ed225d3f30d1ec5033db2be5f
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-06-30 17:04:26 +0700

    refactor: add query method with optimistic locking

--- src/main/java/com/rizalamar/loan_ples/repository/LoanRepository.java
+++ src/main/java/com/rizalamar/loan_ples/repository/LoanRepository.java
@@ -3,7 +3,9 @@ package com.rizalamar.loan_ples.repository;
 import com.rizalamar.loan_ples.domain.Loan;
 import com.rizalamar.loan_ples.domain.LoanStatus;
 import com.rizalamar.loan_ples.domain.User;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,4 +17,7 @@ public interface LoanRepository extends JpaRepository<Loan, UUID> {
     boolean existsByBorrowerAndStatus(User borrower, LoanStatus status);
 
     List<Loan> findByStatus(LoanStatus status);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Loan> findWithLockById(UUID id);
 }
